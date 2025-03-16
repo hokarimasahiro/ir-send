ded3f9345c5f30c6e4bdc3e5ff90f6d42c0e8537
hokarimasahiro <42521359+hokarimasahiro@users.noreply.github.com>
2025-03-16 10:14:23 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,12 @@
 let nextTime = 0
 pins.analogSetPitchPin(AnalogPin.P1)
+let sTime = 100
 basic.showIcon(IconNames.Heart)
 basic.forever(function () {
     if (input.buttonIsPressed(Button.A)) {
         nextTime = input.runningTime() + 1000
         basic.showIcon(IconNames.Diamond)
-        pins.analogPitch(38000, 2000)
+        pins.analogPitch(38000, sTime)
         basic.showIcon(IconNames.SmallDiamond)
         basic.pause(500)
         basic.clearScreen()
