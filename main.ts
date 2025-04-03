ad5825b3c3e586523e6b714e4956d62138e7a210
hokarimasahiro <42521359+hokarimasahiro@users.noreply.github.com>
2025-04-03 19:33:52 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,41 @@
 let nextTime = 0
 pins.analogSetPitchPin(AnalogPin.P1)
 let sTime = 100
-basic.showIcon(IconNames.Heart)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . # . .
+    . . . . .
+    . . . . .
+    `)
 basic.forever(function () {
     if (input.buttonIsPressed(Button.A)) {
         nextTime = input.runningTime() + 1000
-        basic.showIcon(IconNames.Diamond)
+        basic.showLeds(`
+            . . # . .
+            . # . # .
+            # . . . #
+            . # . # .
+            . . # . .
+            `)
         pins.analogPitch(38000, sTime)
-        basic.showIcon(IconNames.SmallDiamond)
+        basic.showLeds(`
+            . . . . .
+            . . # . .
+            . # . # .
+            . . # . .
+            . . . . .
+            `)
         basic.pause(500)
-        basic.clearScreen()
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
         while (input.buttonIsPressed(Button.A) || input.runningTime() < nextTime) {
         	
         }
-        basic.showIcon(IconNames.Heart)
     }
 })
