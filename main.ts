9f7b1c8d51556bd552a773451d179232c102bfe2
hokarimasahiro <42521359+hokarimasahiro@users.noreply.github.com>
2025-07-04 07:39:36 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 let nextTime = 0
 pins.analogSetPitchPin(AnalogPin.P1)
+pins.setPull(DigitalPin.P2, PinPullMode.PullUp)
 let sTime = 50
 let lTime = 1000
 basic.showLeds(`
@@ -10,12 +11,12 @@ basic.showLeds(`
     . # . # .
     `)
 basic.forever(function () {
-    if (input.buttonIsPressed(Button.A)) {
+    if (pins.digitalReadPin(DigitalPin.P2) == 0) {
         nextTime = input.runningTime() + lTime
         led.unplot(2, 2)
         pins.analogPitch(38000, sTime)
     }
-    while (input.buttonIsPressed(Button.A) || input.runningTime() < nextTime) {
+    while (pins.digitalReadPin(DigitalPin.P2) == 0 || input.runningTime() < nextTime) {
     	
     }
     led.plot(2, 2)
